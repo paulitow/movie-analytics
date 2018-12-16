3743938c95a002bc4a134cdaf2075f2c7bc82af5
giteub13 <desmarets-uriarte.damien@orange.fr>
2018-12-16 18:18:40 +0100

    --Definition de la structure film

---
+++ structures.h
@@ -0,0 +1,20 @@
+#ifndef STRUCTURES_H
+#define STRUCTURES_H
+#define MAX_BUFF  20
+
+#include <stdio.h>
+#include <string.h>
+
+typedef struct {
+
+  int id;
+  char nom;
+  int annee;
+  char genre;
+  int duration;
+  char pays;
+  char realisateur;
+  char acteur;
+  float moyenne_notes;
+  float nb_votes;
+}film;
